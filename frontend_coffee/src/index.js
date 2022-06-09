178bc63a123a8dd445d21446045547d4645c15e7
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-09 09:22:46 +0200

    $0025: [Frontend] [AppSetup] [Moralis]: blank page + wrap app in moralis, browser and notification provider

--- frontend_coffee/src/index.js
+++ frontend_coffee/src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { MoralisProvider } from "react-moralis";
+import { BrowserRouter } from "react-router-dom";
+import { NotificationProvider } from "web3uikit";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      appId={process.env.REACT_APP_MORALIS_APP_ID}
+      serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}
+    >
+      <BrowserRouter>
+        <NotificationProvider>
+          <App />
+        </NotificationProvider>
+      </BrowserRouter>
+    </MoralisProvider>
   </React.StrictMode>
 );
 
